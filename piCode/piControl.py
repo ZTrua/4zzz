e70fe07faed3fb49f6c04c5dc9f49bf141196591
zoe Truasheim <zoe.truasheim@gmail.com>
2025-04-13 16:15:00 -0900

    changed piControl

--- piCode/piControl.py
+++ piCode/piControl.py
@@ -1,9 +1,36 @@
 import serial
+import time 
 
 class SerSend:
-    def __init__(self): 
-        pass
+    def __init__(self, serial, addr): 
+        self.ser = serial
+        self.addr = addr
+
+    def stx(self): 
+        self.ser.write("start of text")
+
+    def addr(self, addr): 
+        self.ser.write(self.addr)
+
+    def seq(self):
+        self.ser.write("seq")
+    
+    def etx(self): 
+        self.ser.write("end of text")
+
+    def send_data(self, message):
+        self.stx() 
+        self.addr() 
+        self.seq() 
+        self.seq.write(message)
+        self.etx() 
 
 class SerGet: 
     def __init__(self):
-        pass
+        stx = False
+        addr = False
+        seq = False 
+        ext = False 
+        stx_string = "start of text" 
+        ext_string = "end of text"
+        expected = ["stx", "addr", "seq", "message", "ext"] 
