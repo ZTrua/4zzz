41374d862f0aa14c210bc01e334e0664abf1c718
zoe Truasheim <zoe.truasheim@gmail.com>
2025-04-20 00:48:42 -0900

    changed some python stuff

--- piCode/piControl.py
+++ piCode/piControl.py
@@ -1,6 +1,10 @@
 import serial
 import time 
 
+class serMessage:
+    def __init__(self): 
+        pass
+
 class SerSend:
     def __init__(self, serial, addr): 
         self.ser = serial
@@ -29,8 +33,34 @@ class SerGet:
     def __init__(self):
         stx = False
         addr = False
-        seq = False 
+        seq_flag = False 
         ext = False 
         stx_string = "start of text" 
         ext_string = "end of text"
-        expected = ["stx", "addr", "seq", "message", "ext"] 
+        state = ["stx", "addr", "seq", "message", "ext"] 
+        current_state = 0
+        seq = 361
+
+    def update_seq(self): 
+        # increase sequence number with 360 rollover 
+
+        if self.seq == 361: 
+            self.seq = 0
+        elif self.seq == 360: 
+            self.seq = 0
+        elif self.seq > 361: 
+            self.ser_error("seq error", "too high")
+        else: 
+            self.seq = self.seq + 1
+
+        return self.seq
+        
+    
+    def read_serial(self, message): 
+        # read the message recieved by the serial and interpret it 
+        if self.message == self.stx_string: 
+            pass
+
+    def ser_error(self, message, details): 
+        pass
+    
